ba74a62f9845a363948fa56d4a370313c561769e
the-camputer <mynichor@gmail.com>
2020-09-19 12:01:10 -0400

    13 added notes on queues

--- ccc-13.cpp
+++ ccc-13.cpp
@@ -8,6 +8,7 @@
 #include <array>
 #include <vector>
 #include <stack>
+#include <queue>
 
 /*
     Arrays: RAII wrapper around statically-sized arrays.
@@ -166,3 +167,26 @@ TEST_CASE("std::stack") {
         REQUIRE(easy_as.empty());
     }
 }
+
+TEST_CASE("std::queue supports push/pop/front/back") {
+    std::deque<int> deq { 1, 2 };
+    std::queue<int> easy_as(deq);
+
+    REQUIRE(easy_as.front() == 1);
+    REQUIRE(easy_as.back() == 2);
+
+    easy_as.pop();
+    easy_as.push(3);
+
+    REQUIRE(easy_as.front() == 2);
+    REQUIRE(easy_as.back() == 3);
+
+    easy_as.pop();
+
+    REQUIRE(easy_as.front() == 3);
+
+    easy_as.pop();
+
+    REQUIRE(easy_as.empty());
+}
+
