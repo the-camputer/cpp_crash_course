ed48aeb75f7425fc8ee5cfbc4f30e293e4deb19d
the-camputer <mynichor@gmail.com>
2020-10-16 22:43:51 -0400

    19 Added notes on task errors and waiting

--- ccc-19.cpp
+++ ccc-19.cpp
@@ -28,3 +28,24 @@ TEST_CASE("async valid() defaults to false") {
     future<bool> fake_future;
     REQUIRE_FALSE(fake_future.valid());
 }
+
+TEST_CASE("future will throw any errors the async task throws") {
+    auto explosion = async([] { throw runtime_error { "Boom." }; });
+    REQUIRE_THROWS_AS(explosion.get(), runtime_error);
+}
+
+/*
+    You can also wait for async tasks to finish using wait_for(Duration) or wait_until(time_point)
+    result:
+        - future_status::deferred - if the async task is deffered
+        - future_status::ready - completed and results are ready
+        - future_status::timeout - result not yet ready
+*/
+TEST_CASE("Waiting for a task to complete") {
+    using namespace literals::chrono_literals;
+    auto sleepy = async(launch::async, [] { this_thread::sleep_for(100ms); });
+    const auto not_ready_yet = sleepy.wait_for(25ms);
+    REQUIRE(not_ready_yet == future_status::timeout);
+    const auto ready_now = sleepy.wait_for(100ms);
+    REQUIRE(ready_now == future_status::ready);
+}
